26e043e90528a9b7d54e8555d856e96b22c375a6
wangqianjun <wangqianjun@wangqianjundeMacBook-Pro.local>
2014-08-31 15:45:07 +0800

    add some codes

--- UIKit-UIAccelerometer/ViewController.m
+++ UIKit-UIAccelerometer/ViewController.m
@@ -8,24 +8,34 @@
 
 #import "ViewController.h"
 
-@interface ViewController (private)
+double currentMaxAccelX;
+double currentMaxAccelY;
+double currentMaxAccelZ;
 
-
-
-@end
-
-@implementation ViewController (private)
-
-
-
-@end
-
-
+double currentMaxRotX;
+double currentMaxRotY;
+double currentMaxRotZ;
 
 @interface ViewController ()
 
 @property(nonatomic, strong)CMMotionManager *motionManager;
+@property (strong, nonatomic) IBOutlet UILabel *accX;
+@property (strong, nonatomic) IBOutlet UILabel *accY;
+@property (strong, nonatomic) IBOutlet UILabel *accZ;
 
+@property (strong, nonatomic) IBOutlet UILabel *rotX;
+@property (strong, nonatomic) IBOutlet UILabel *rotY;
+@property (strong, nonatomic) IBOutlet UILabel *rotZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxAccX;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccY;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxRotX;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotY;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotZ;
+
+- (IBAction)resetMaxValues:(id)sender;
 
 @end
 
@@ -35,25 +45,96 @@
 {
     [super viewDidLoad];
     
+    currentMaxAccelX = 0;
+    currentMaxAccelY = 0;
+    currentMaxAccelZ = 0;
+    
+    currentMaxRotX = 0;
+    currentMaxRotY = 0;
+    currentMaxRotZ = 0;
+    
+    self.motionManager = [[CMMotionManager alloc]init];
+    self.motionManager.accelerometerUpdateInterval = 0.2;
+    self.motionManager.gyroUpdateInterval = 0.2;
+    
+    [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+        [self outputAccelerationData:accelerometerData.acceleration];
+        if (error) {
+            NSLog(@"%@",error.localizedDescription);
+        }
+    }];
+    
+    
+    [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMGyroData *gyroData, NSError *error) {
+        [self outputRotationData:gyroData.rotationRate];
+        if (error) {
+            NSLog(@"%@",error.localizedDescription);
+        }
+    }];
     
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)initMotion
+- (void)outputAccelerationData:(CMAcceleration)acceleration
 {
-    self.motionManager = [[CMMotionManager alloc]init];
+    self.accX.text = [NSString stringWithFormat:@"%0.2f",acceleration.x];
+    if (fabs(acceleration.x) > fabs(currentMaxAccelX)) {
+        currentMaxAccelX = fabs(acceleration.x);
+    }
+    self.accY.text = [NSString stringWithFormat:@" %.2fg",acceleration.y];
+    if(fabs(acceleration.y) > fabs(currentMaxAccelY))
+    {
+        currentMaxAccelY = acceleration.y;
+    }
+    self.accZ.text = [NSString stringWithFormat:@" %.2fg",acceleration.z];
+    if(fabs(acceleration.z) > fabs(currentMaxAccelZ))
+    {
+        currentMaxAccelZ = acceleration.z;
+    }
+    
+    self.maxAccX.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelX];
+    self.maxAccY.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelY];
+    self.maxAccZ.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelZ];
     
 }
 
-- (void)accelerometer
+- (void)outputRotationData:(CMRotationRate)rotation
 {
+    self.rotX.text = [NSString stringWithFormat:@" %.2fr/s",rotation.x];
+    if(fabs(rotation.x) > fabs(currentMaxRotX))
+    {
+        currentMaxRotX = rotation.x;
+    }
+    self.rotY.text = [NSString stringWithFormat:@" %.2fr/s",rotation.y];
+    if(fabs(rotation.y) > fabs(currentMaxRotY))
+    {
+        currentMaxRotY = rotation.y;
+    }
+    self.rotZ.text = [NSString stringWithFormat:@" %.2fr/s",rotation.z];
+    if(fabs(rotation.z) > fabs(currentMaxRotZ))
+    {
+        currentMaxRotZ = rotation.z;
+    }
     
+    self.maxRotX.text = [NSString stringWithFormat:@" %.2f",currentMaxRotX];
+    self.maxRotY.text = [NSString stringWithFormat:@" %.2f",currentMaxRotY];
+    self.maxRotZ.text = [NSString stringWithFormat:@" %.2f",currentMaxRotZ];
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)resetMaxValues:(id)sender {
+    
+    currentMaxAccelX = 0;
+    currentMaxAccelY = 0;
+    currentMaxAccelZ = 0;
+    
+    currentMaxRotX = 0;
+    currentMaxRotY = 0;
+    currentMaxRotZ = 0;
+    
+}
 @end
