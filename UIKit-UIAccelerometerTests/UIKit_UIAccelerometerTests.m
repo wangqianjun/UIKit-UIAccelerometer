a634d4018ba701d806c90fe6925f3ff730c47f78
王钱钧 <wangqianjun@wangqianjundeMacBook-Pro.local>
2014-08-31 09:21:51 +0800

    Initial Commit

---
+++ UIKit-UIAccelerometerTests/UIKit_UIAccelerometerTests.m
@@ -0,0 +1,34 @@
+//
+//  UIKit_UIAccelerometerTests.m
+//  UIKit-UIAccelerometerTests
+//
+//  Created by 王钱钧 on 14-8-31.
+//  Copyright (c) 2014年 王钱钧. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIKit_UIAccelerometerTests : XCTestCase
+
+@end
+
+@implementation UIKit_UIAccelerometerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
